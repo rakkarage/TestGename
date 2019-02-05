1c43d0758d78e85ce989153b2efb45f8d73a28b1
Rakka Rage <rakkarage@gmail.com>
2019-02-04 21:42:11 -0500

    x

--- TestGename/wwwroot/js/site.js
+++ TestGename/wwwroot/js/site.js
@@ -2,14 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-var video = document.querySelector("#videoElement");
-
-if (navigator.mediaDevices.getUserMedia) {
-	navigator.mediaDevices.getUserMedia({ video: true })
-		.then(function (stream) {
-			video.srcObject = stream;
-		})
-		.catch(function (err0r) {
-			console.log("Something went wrong!");
-		});
-}
